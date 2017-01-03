0a455bd84ff9875f6c6b050dc70cedba8dbc66f7
Brian Olson <brian.olson@gmail.com>
2017-01-03 01:53:28 -0500

    serve from immediate grab from subprocess cache-control

--- servetimelapse.py
+++ servetimelapse.py
@@ -2,10 +2,14 @@
 #
 # manage output of raspistill command like:
 # raspistill -tl 30000 -n -dt -o /mnt/usb128/timelapse/i%d.jpg -t 0 -w 1920 -h 1080 -vf -hf
+#
+# TODO: directly capture from camera STDOUT
+# raspistill -o - -t 10 -vf -hf -w 1920 -h 1080 -n
 
 import glob
 import json
 import os
+import subprocess
 import sys
 import threading
 import time
@@ -49,9 +53,25 @@ def serveLatestImage(environ, start_response):
     return serveImage(environ, start_response, imgPath, noCache=True)
 
 
+def imageNowFromSubprocess():
+    cmd = ['raspistill', '-o', '-', '-t', '10', '-vf', '-hf', '-w', '1920', '-h', '1080', '-n']
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    pout, perr = proc.communicate(timeout=23)
+    if proc.returncode != 0:
+        sys.stderr.write('return code {} from subprocess {:r}\n'.format(proc.returncode, cmd))
+        raise Exception("raspistill failed")
+    return pout
+
+# for the image
+CACHE_SECONDS = 7
+
+PAGE_CACHE_HEADER = ('Cache-control', 'max-age=30')
+
 class GardenServer(object):
     def __init__(self, tempHumid):
         self.tempHumid = tempHumid
+        self.imageCache = None
+        self.imageTimestamp = None
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
@@ -68,10 +88,14 @@ class GardenServer(object):
         if path == '/th.js':
             return self.serveTempHumid(environ, start_response)
 
-        return self.serveList(environ, start_response, limit=50)
+        if path == '/grab.jpg':
+            return self.serveImmediate(environ, start_response)
+
+        #return self.serveList(environ, start_response, limit=50)
+        return self.serveImmediateHome(environ, start_response)
 
     def serveTempHumid(self, environ, start_response):
-        start_response('200 OK', [('Content-type', 'application/json; charset=utf-8')])
+        start_response('200 OK', [('Content-type', 'application/json; charset=utf-8'), PAGE_CACHE_HEADER])
         timeTempHumid = self.tempHumid.getLatest()
         out = {}
         if timeTempHumid is not None:
@@ -79,8 +103,24 @@ class GardenServer(object):
             out['tempHumids'] = timeTempHumid.tempHumids
         yield json.dumps(out).encode('utf8')
 
+    def serveImmediateHome(self, environ, start_response):
+        start_response('200 OK', [('Content-type', 'text/html; charset=utf-8'), PAGE_CACHE_HEADER])
+        yield b'''<!DOCTYPE html>
+<html>
+<head>
+	<title>bpi3 timelapse</title>
+	<meta charset="utf-8" />
+</head>
+<body style="background-color:#fff;">
+'''
+        timeTempHumid = self.tempHumid.getLatest()
+        if timeTempHumid is not None:
+            yield ('<div>{:0.1f} °C {:0.1f} °F {:0.1f}% humidity</div>\n'.format(timeTempHumid.degC(), timeTempHumid.degF(), timeTempHumid.pctHumid())).encode('utf8')
+        yield b'<a href="/grab.jpg"><div>current</div><div><img src="/grab.jpg" width="480" height="270" /></div></a>\n'
+        yield b'</body></html>\n'
+
     def serveList(self, environ, start_response, limit=None):
-        start_response('200 OK', [('Content-type', 'text/html; charset=utf-8')])
+        start_response('200 OK', [('Content-type', 'text/html; charset=utf-8'), PAGE_CACHE_HEADER])
         yield b'''<!DOCTYPE html>
 <html>
 <head>
@@ -101,6 +141,24 @@ class GardenServer(object):
                 yield b'<div><a href="/list/" style="font-size: 130%">...</a></div>\n'
                 break
         yield b'</body></html>\n'
+
+    def serveImmediate(self, environ, start_response):
+        data = None
+        now = time.time()
+        if self.imageCache is not None:
+            dt = now - self.imageTimestamp
+            if dt < CACHE_SECONDS:
+                data = self.imageCache
+        
+        if data is None:
+            data = imageNowFromSubprocess()
+            self.imageCache = data
+            self.imageTimestamp = now
+        start_response('200 OK', [
+            ('Content-type', 'image/jpeg'),
+            ('Cache-control', 'max-age={}'.format(CACHE_SECONDS))
+        ])
+        return [data]
 
 
 def main():
